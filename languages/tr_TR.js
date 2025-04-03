7cc65baf96548b273fc68bafba928f1392c158ed
Talha Tufan <talha.tufan@hotmail.com>
2025-04-04 00:36:37 +0300

    Türkçe dil desteği eklendi.

---
+++ languages/tr_TR.js
@@ -0,0 +1,19 @@
+export const weatherTranslationsTr = {
+    'Partly cloudy': 'Parçalı bulutlu',
+    'Partly Cloudy ': 'Parçalı bulutlu',
+    'Moderate rain': 'Orta şiddetli yağmur',
+    'Patchy rain possible': 'Yer yer yağmur olasılığı',
+    'Patchy rain nearby': 'Yakınlarda yer yer yağmur',
+    'Sunny': 'Güneşli',
+    'Clear': 'Açık',
+    'Overcast': 'Kapalı',
+    'Cloudy': 'Bulutlu',
+    'Light rain': 'Hafif yağmur',
+    'Moderate rain at times': 'Zaman zaman orta şiddetli yağmur',
+    'Heavy rain': 'Şiddetli yağmur',
+    'Heavy rain at times': 'Zaman zaman şiddetli yağmur',
+    'Moderate or heavy freezing rain': 'Orta veya şiddetli dondurucu yağmur',
+    'Moderate or heavy rain shower': 'Orta veya şiddetli sağanak yağış',
+    'Moderate or heavy rain with thunder': 'Gök gürültülü yağmur',
+    'other': 'Diğer'
+  };
